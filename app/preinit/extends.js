c12419ee00f782b174ddd0aba28d5e8570321014
James Bailey <jimi.bailey@gmail.com>
2013-01-25 06:49:49 +0000

    Added extension to Array forEachAsync

--- app/preinit/extends.js
+++ app/preinit/extends.js
@@ -3,4 +3,23 @@ module.exports = function(app){
 		var regexp = new RegExp(":"+param, "g");
 		return this.toString().replace(regexp, value);
 	}
+	
+	Array.prototype.forEachAsync = function(action, cb){
+		var self = this;
+		var i = - 1;
+		var lim = this.length;
+
+		next();
+
+		function next(){
+			if(++i < lim){
+				action(self[i], function(err, result){
+					self[i] = result || self[i];
+					next();
+				});
+			}else{
+				cb();
+			}
+		}
+	}
 }
